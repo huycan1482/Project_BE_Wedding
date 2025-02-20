42a372ed40b9d116ff692365f58a46a39cae898b
huycan1482 <huycan1482@gmail.com>
2025-02-20 22:05:45 +0700

    f

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -9,6 +9,26 @@ use Illuminate\Support\Facades\Mail;
 
 class SendMailController extends Controller
 {
+    public function test (Request $request) 
+    {
+        // $a = 5;
+        // $b = &$a;  // $b trỏ đến cùng vùng nhớ với $a
+        // $b = 10;   // Thay đổi $b cũng ảnh hưởng đến $a
+        // echo $a;  // Kết quả: 10
+
+        // $arr = [1, 2, 3];
+        // foreach ($arr as &$value) {
+        //     $value *= 2;
+        // }
+        // print_r($arr); // Kết quả: [2, 4, 6]
+
+        // $x = 3 + "15%" + "$25" = 18;
+
+        $text = 'Hello ';
+        $text[10] = 'world';
+        dd($text);
+    }
+
     public function sendMail (SendMailRequest $request) 
     {
         $order = [
--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/test', [SendMailController::class, 'test']);
+
 Route::post('/send-mail', [SendMailController::class, 'sendMail']);
